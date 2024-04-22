5fb97beec515dc6746ebaba885cbcdc6850d0637
SUBROTOKOIRY <koirysubroto1103@gmail.com>
2024-04-23 03:06:18 +0530

    Improved the patient appointment response API so that it make a request to both doctor & patient server

--- controllers/dueAppointments.js
+++ controllers/dueAppointments.js
@@ -1,30 +1,57 @@
 const mongoose = require('mongoose')
 
-
 const { patientTracksSchema } = require('../models/patientTracksSchema')
 
+// checks all the pending appointments requests from different patients and their doctor allocations
 const pendingAppointmentsConfrimations = async (req, res) => {
-  const patinetQueue = mongoose.model('patientTracks', patientTracksSchema)
   try {
     const hospital_id = req.params.hospital_id
-    const pending_requests_confirmations = await patinetQueue.find({
+
+    const patientQueue = mongoose.model('patientTracks', patientTracksSchema)
+    
+    const pending_requests_confirmations = await patientQueue.find(
+      {
+          is_appointment_alloted: false,
+          "hospital_id_name.0": hospital_id,
+          doctor_id_name_department: { $ne: []},
+      }
+    )
+                              //OR
+    // const pending_requests_confirmations = await patientQueue.aggregate([
+    //   {
+    //     $match: {
+    //       is_appointment_alloted: false,
+    //       'hospital_id_name.0': hospital_id,
+    //       doctor_id_name_department: { $ne: [] },
+    //     },
+    //   },
+    // ])
+
+    const pending_doctor_allocations = await patientQueue.find({
       is_appointment_alloted: false,
-      hospital_id,
-    })
-    const pending_doctor_allocations = await patinetQueue.find({
-      is_appointment_alloted: true,
-      doctor_id: null,
+      'hospital_id_name.0': hospital_id,
+      doctor_id_name_department: { $eq: [] },
     })
 
     res
       .status(200)
       .json({ pending_requests_confirmations, pending_doctor_allocations })
   } catch (err) {
+    console.log(err.message)
     res.status(500).json({ message: err.message })
   }
 }
 
+const allocateDoctors=async(req,res)=>{
+  try{
+    
+  }
+  catch(err){
+    console.log(err.message);
+    res.status(500).json({message:err.message});
+  }
+}
 
 module.exports = {
-  pendingAppointmentsConfrimations
+  pendingAppointmentsConfrimations,
 }
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,15 +3,22 @@ const router = require('express').Router()
 const {
   patientBookingRequests,
   patientAppointmentResponse,
+  fetchHospitalsCardDetails,
+  fetchHospitalDetails,
 } = require('../controllers/patientBackendRequests')
+
 const {
   pendingAppointmentsConfrimations,
 } = require('../controllers/dueAppointments')
 
+
 router.route('/bookingRequest').post(patientBookingRequests)
-router
-  .route('/pendingRequests/:hospital_id')
-  .get(pendingAppointmentsConfrimations)
 router.route('/bookingResponse/:hospital_id').post(patientAppointmentResponse)
+
+
+router.route('/pendingRequests/:hospital_id').get(pendingAppointmentsConfrimations)
+
+router.route('/fetchHospitalsCardDetails').post(fetchHospitalsCardDetails)
+router.route('/fetchHospitalDetails/:hospital_id').post(fetchHospitalDetails)
 
 module.exports = router
